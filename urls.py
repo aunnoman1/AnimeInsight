601f5111d27d86b673301a13abbe6fb43203ac5f
M-Haseeb01 <153316025+M-Haseeb01@users.noreply.github.com>
2024-04-28 22:04:57 +0500

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from . import views
-from .views import add_to_cart
 
 urlpatterns = [
-    path('', views.index, name='index'),  # This URL pattern is already defined
-     path('add-to-cart/', add_to_cart, name='add_to_cart'),
-       path('cart/', views.view_cart, name='view_cart'),
-      path('empty-cart/', views.empty_cart, name='empty_cart'),
-    path('anime/', views.index, name='anime_list'),  # Add this line for the anime list view
+    path('', views.index, name='index'),  # Existing URL pattern
+    path('add-to-wishlist/<int:anime_id>/', views.add_to_wishlist, name='add_to_wishlist'),  # New URL pattern
+     path('wishlist/', views.view_wishlist, name='wishlist'),
+     path('remove-from-wishlist/<int:anime_id>/', views.remove_from_wishlist, name='remove_from_wishlist'),
 ]
